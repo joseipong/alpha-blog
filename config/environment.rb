40ac462031594fef6222c1ff45228ce0be210ec6
Jose Martin Ipong <ijm29game@gmail.com>
2020-07-13 18:00:38 +0800

    Add bootstrap styling to pages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disable rails adding fields_with_error div on form validation error
+# fields_with_error div messes up the styling of the form
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
